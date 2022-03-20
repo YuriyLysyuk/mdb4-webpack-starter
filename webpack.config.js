11ef987b5fa75af31b3eb7eed421a4054100d754
lysyuk <lysyuk.y@gmail.com>
2022-03-20 12:57:07 +0300

    config(webpack): add initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,132 @@
+const path = require('path');
+const webpack = require('webpack');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const optimization = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all',
+    },
+  };
+
+  if (isProd) {
+    config.minimizer = [
+      new TerserPlugin({ extractComments: false }),
+      new CssMinimizerPlugin(),
+    ];
+    config.minimize = true;
+  }
+
+  return config;
+};
+
+const fileName = (ext) =>
+  isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
+
+const cssLoaders = (extra) => {
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+    },
+    'css-loader',
+  ];
+
+  if (extra) {
+    loaders.push(extra);
+  }
+
+  return loaders;
+};
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    // main: ['./js/main.js', './scss/main.scss'],
+    main: './js/main.js',
+  },
+  output: {
+    filename: `js/${fileName('js')}`,
+    path: path.resolve(__dirname, 'dist'),
+    asyncChunks: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  optimization: optimization(),
+  devServer: {
+    port: 9000,
+    hot: isDev,
+  },
+  devtool: isDev ? 'source-map' : false,
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './index.html',
+      inject: 'body',
+      minify: {
+        collapseWhitespace: isProd,
+      },
+    }),
+    new CleanWebpackPlugin(),
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: path.resolve(__dirname, 'src/favicon.ico'),
+    //     to: path.resolve(__dirname, 'dist'),
+    //   },
+    // ]),
+    new MiniCssExtractPlugin({
+      filename: `css/${fileName('css')}`,
+    }),
+    // new webpack.ProvidePlugin({
+    //   $: 'jquery',
+    //   jQuery: 'jquery',
+    // }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: cssLoaders(),
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader'),
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name]-[contenthash][ext][query]',
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime'],
+          },
+        },
+      },
+      {
+        test: /\.(svg|eot|woff|woff2|ttf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name]-[contenthash][ext][query]',
+        },
+      },
+    ],
+  },
+};
